ff0e7f6908ef4fdcb78e64fb627b5cbbed18c247
unknown <raphael.malburg@gmail.com>
2023-01-31 16:26:04 +0000

    form

--- index.js
+++ index.js
@@ -1,18 +1,59 @@
-function validateFields(){
+function onChangeEmail(){
+   toggleButtonsDisable();
+   toggleEmailErrors();
+   
+}
+
+function onChangePassword(){
+    toggleButtonsDisable();
+    togglePasswordErrors();
+}
+
+function toggleEmailErrors(){
+    const email = form.email().value;
+    form.emailRequiredError().style.display = email ? 'none' : 'block'
+   
+    form.emailInvalidError().style.display = validateEmail(email) ? 'none' : 'block'
+
+
+}
+
+function toggleButtonsDisable(){
     const emailValid = isEmailValid();
-    const recoverButton = document.getElementById('recover-password-button')
+    const recoverButton = form.recoverPassword()
+    const loginButton = form.loginButton()
+    const passwordValid = isPasswordValid();
+
     if(emailValid){
         recoverButton.removeAttribute('disabled')
     }else{
         recoverButton.setAttribute('disabled','')
     }
+
     
-     
+    if(!emailValid || !passwordValid){
+        loginButton.setAttribute('disabled','')
+    }else  {
+        loginButton.removeAttribute('disabled')  
+    }
+}
+
+function togglePasswordErrors(){
+    const password = form.password().value;
+    form.passwordRequiredError().style.display = password ? 'none' : 'block';
     
 }
 
+function isPasswordValid(){
+    const password = form.password().value;
+    if(!password){
+        return false
+    }
+    return true;
+}
+
 function isEmailValid(){
-    const email = document.getElementById('email').value;
+    const email = form.email().value;
     if (!email){
         return false;
     }
@@ -22,4 +63,14 @@ function isEmailValid(){
 function  validateEmail(email){
     return  /\S+@\S+\.\S+/.test(email)
     
+}
+
+const form = {
+    email:() => document.getElementById('email'),
+    password: ()=>  document.getElementById('password'),
+    loginButton: () => document.getElementById('login-button'),
+    recoverPassword: () => document.getElementById('recover-password-button'),
+    emailInvalidError: () => document.getElementById('email-invalid-error'),
+    passwordRequiredError: () =>  document.getElementById('password-required-error'),
+    emailRequiredError: () => document.getElementById('email-required-error')
 }
